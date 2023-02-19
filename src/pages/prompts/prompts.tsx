893a66a389748a3814083f3823d3960a7b4ec9d7
codetunez <codetunez@outlook.com>
2023-02-19 14:54:20 -0800

    CSS Cleanup

--- src/pages/prompts/prompts.tsx
+++ src/pages/prompts/prompts.tsx
@@ -18,15 +18,7 @@ import Header from '../../core/ui/header';
 export default function Prompts() {
 
   const gptContext: any = React.useContext(GptContext);
-  const [models, setModels] = React.useState([]);
   const [apis, setApis] = React.useState([]);
-
-  React.useEffect(() => {
-    const items = gptContext.models && gptContext.models.map((element: any) => {
-      return { name: element.id, value: element.id }
-    })
-    setModels(items);
-  }, [gptContext.models]);
 
   React.useEffect(() => {
     const items = gptContext.apis && gptContext.apis.map((element: any) => {
@@ -40,34 +32,30 @@ export default function Prompts() {
     {gptContext.error ? <Modal><h2>{gptContext.error}</h2></Modal> : null}
     {gptContext.loading ? <Modal><h2>{gptContext.currentModel !== '' ? "asking..." : "asking for models"}</h2><br /><PuffLoader color="#fff" /></Modal> : null}
 
-    <div className="header-bar"><Header/></div>
+    <div className="header-bar"><Header /></div>
 
     <div className="prompts">
-      <div className="panel panel-history">
-        <div className="panel-header"><h3>History</h3></div>
-        <div className="panel-content">
-          <div className="panel-content-inner">
-            <History />
-          </div>
+
+      <div className="panel prompts-history">
+        <div className="panel-toolbar">
+          <h3>History</h3>
         </div>
+        <History />
       </div>
 
-      <div className="panel panel-gpt">
-        <div className="panel-header">
+      <div className="panel prompts-gpt">
+        <div className="panel-toolbar">
           <h3>Prompt</h3>
           <div className='btn-bar'>
-            <button className='button-primary' onClick={() => gptContext.newQuery()}>New</button>
+            <div>Service</div>
             <Combo items={apis} value={gptContext.currentApi} onChange={(e: any) => { gptContext.setCurrentApi(e.target.value) }} />
-            <Combo items={models} value={gptContext.currentModel} onChange={(e: any) => { gptContext.setCurrentModel(e.target.value) }} />
+            <button className='button-primary' onClick={() => gptContext.newQuery()}>Clear</button>
           </div>
         </div>
-        <div className="panel-content">
-          <div className="panel-content-inner">
-            <QueryInput />
-            <Results />
-          </div>
-        </div>
+        <QueryInput />
+        <Results />
       </div>
+
     </div>
 
   </div>;
--- src/pages/prompts/queryInput.tsx
+++ src/pages/prompts/queryInput.tsx
@@ -1,26 +1,30 @@
 import './queryInput.css';
 import React from 'react';
-import { CSSProperties } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
 import { GptContext } from '../../context/gptContext';
+import { Combo } from "../../core/ui/controls";
 
 export default function QueryInput() {
 
     const gptContext: any = React.useContext(GptContext);
+    const [models, setModels] = React.useState([]);
 
-    const override: CSSProperties = {
-        margin: "0 auto",
-        borderColor: "white",
-        width: "10px",
-        height: "10px"
-    };
-
-    <ClipLoader color="#33C3F0" loading={gptContext.loading} cssOverride={override} />
+    React.useEffect(() => {
+        const items = gptContext.models && gptContext.models.map((element: any) => {
+            return { name: element.id, value: element.id }
+        })
+        setModels(items);
+    }, [gptContext.models]);
 
     return <div className="query">
         <textarea placeholder='Ask a question' name="query" value={gptContext.currentPrompt || ''} onChange={(e: any) => { gptContext.setCurrentPrompt(e.target.value) }}></textarea>
-        <button className='button-primary' onClick={() => gptContext.executeQuery()}>Ask</button>
+        <div className="panel-toolbar">
+            <div className='btn-bar'>
+                <div>Model</div>
+                <Combo items={models} value={gptContext.currentModel} onChange={(e: any) => { gptContext.setCurrentModel(e.target.value) }} />
+            </div>
+            <div className='btn-bar'>
+                <button className='button-primary' onClick={() => gptContext.executeQuery()}>Complete</button>
+            </div>
+        </div>
     </div>
 }
-
-
