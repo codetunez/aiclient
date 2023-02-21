6e711e5fd1475e380e3dd38ab2ff25421f00ec51
codetunez <codetunez@outlook.com>
2023-02-21 11:27:26 -0800

    Deployment fixes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// This www server can be used instead of npm run start
 var express = require('express')
 
 var app = express()
-var port = 3000
+var port = process.env.port || 3000;
 
 app.use(express.static('build'));
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + '/build/index.html')));
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
+    console.log(`AI client listening on port: ${port}`)
 })
